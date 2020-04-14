40f3aba76ca6d66e4009c3e2e51b8a769f890fed
Romain <romain.caneill@yahoo.fr>
2020-04-14 14:50:46 +0200

    merge tool + metrics

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@
 from setuptools import setup, find_packages
 
 DISTNAME = "xnemogcm"
-VERSION = "0.1.0"
+VERSION = "0.1.1"
 LICENSE = "MIT"
 AUTHOR = "Romain Caneill"
 AUTHOR_EMAIL = "romain.caneill@gu.se"
