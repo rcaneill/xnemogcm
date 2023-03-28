001610699b408c21368d876e7761a5e865ba49a2
Romain <romain.caneill@yahoo.fr>
2023-03-28 14:13:47 +0200

    remove old commented line

--- xnemogcm/domcfg.py
+++ xnemogcm/domcfg.py
@@ -12,7 +12,6 @@ def domcfg_preprocess(ds):
     """
     # nemo 3.6
     if "z" in ds or "z" in ds.dims or "z" in ds.coords:
-        # ds = ds.rename({"z": "nav_lev"})
         ds = ds.swap_dims({"z": "nav_lev"})
     if "DOMAIN_position_first" in ds.attrs.keys():
         (x0, y0) = ds.attrs["DOMAIN_position_first"]
