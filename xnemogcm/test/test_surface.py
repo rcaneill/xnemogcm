2986df7aa7fcaa358c71274b3284fb5ec51a58b4
Romain <romain.caneill@yahoo.fr>
2023-08-04 17:46:24 +0200

    add coordinates in nemo dataset, from domcfg

--- xnemogcm/test/test_surface.py
+++ xnemogcm/test/test_surface.py
@@ -12,3 +12,16 @@ def test_open_nemo_surface(data_path):
         datadir=data_path / "surface_fields",
         domcfg=domcfg,
     )
+
+
+def test_open_nemo_surface_coordinates(data_path):
+    """Test opening of nemo surface files (no depth dimension)"""
+    domcfg = open_domain_cfg(
+        datadir=data_path / "mesh_mask_1_file",
+    )
+    nemo_ds = open_nemo(
+        datadir=data_path / "surface_fields",
+        domcfg=domcfg,
+    )
+    assert "glamt" in nemo_ds.sst.coords
+    assert not ("gdept" in nemo_ds.sst.coords)
